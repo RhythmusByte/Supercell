4329f2c590692b626d0c68da72e31d711fe977a6
Akhil Mahesh <akhilmahesh@proton.me>
2024-05-04 22:33:35 +0530

    Responsive Navbar

---
+++ main.js
@@ -0,0 +1,15 @@
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
